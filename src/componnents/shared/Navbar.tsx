115c8132dada85da9104be382ee027460d747b52
unknown <Limon>
2026-09-25 02:43:19 +0600

    updated the navbar

--- src/componnents/shared/Navbar.tsx
+++ src/componnents/shared/Navbar.tsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
+
 const Navbar = () => {
   const links = (
-    <ul className="menu menu-horizontal px-1">
+    <>
       <li>
-        <a>Workouts</a>
+        <Link href="/">Workouts</Link>
       </li>
       <li>
-        <a>My plan</a>
+        <Link href="/my-plan">My Plan</Link>
       </li>
-    </ul>
+    </>
   );
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm px-4 md:px-8">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -22,29 +25,39 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
+              />
             </svg>
           </div>
+
           <ul
             tabIndex={-1}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
           >
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">FITLOG</a>
+
+        <Link href="/" className="btn btn-ghost text-xl font-black">
+          FITLOG
+        </Link>
       </div>
-      <div className="navbar-center hidden lg:flex"></div>
-      {links}
-      <div className="navbar-end">
-        <a className="btn">Plan</a>
-        <a className="btn">Saved</a>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+
+      <div className="navbar-end gap-2">
+        <Link href="/my-plan" className="btn btn-sm">
+          Plan
+        </Link>
+        <Link href="/my-plan" className="btn btn-sm">
+          Saved
+        </Link>
       </div>
     </div>
   );
